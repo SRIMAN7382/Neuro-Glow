1a6c1689148419fe10bfdfbe2baddb57aef4af8f
N.SRIMANNARAYANA REDDY <101311540+SRIMAN7382@users.noreply.github.com>
2025-05-17 15:59:06 +0530

    Clean up application and fix errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   webpack: (config, { dev, isServer }) => {
     // Disable webpack cache in development to prevent file system issues
     if (dev) {
